d7d176c66e45834f4debd9497d3fa15ba19fb7ae
sidinsomniac <chatterjeesid1993@gmail.com>
2021-08-10 22:08:42 +0530

    Add post liking/disliking functionality

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,6 +1,6 @@
 const Post = require('../../models/Post');
 const checkAuth = require("../../utils/check-auth");
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 
 module.exports = {
     Mutation: {
@@ -22,6 +22,46 @@ module.exports = {
                     username,
                     createdAt: new Date().toISOString()
                 });
+                await post.save();
+                return post;
+            } else {
+                throw new UserInputError('Post not found');
+            }
+        },
+        deleteComment: async (_, { postId, commentId }, context) => {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if (post) {
+                const commentIndex = post.comments.findIndex(comment => comment.id === commentId);
+
+                if (post.comments[commentIndex].username === username) {
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not permitted');
+                }
+            } else {
+                throw new UserInputError('Post not found');
+            }
+        },
+        likePost: async (_, { postId }, context) => {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if (post) {
+                const alreadyLiked = post.likes.find(like => like.username === username);
+                if (alreadyLiked) {
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
                 await post.save();
                 return post;
             } else {
